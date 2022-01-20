69eaf43989e0c97e8fa33b1b84912964734f1981
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-20 19:18:13 +0000

    Installed and cleaned the tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
-const sharp = require('sharp');
+import sharp from 'sharp';
 
 const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
