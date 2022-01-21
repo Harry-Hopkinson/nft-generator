037c2b0ac8b48cbbd80c218f333259079a263c0d
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-21 18:59:41 +0000

    Removed images

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
 const sharp = require('sharp');
+const readline = require("readline");
 
 const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
